84325c606c488ad4148f010e59ad087c03bf6030
Heather Amler <heatheramler@yahoo.com>
2014-06-03 00:18:01 -0400

    looping through divs working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,52 +14,63 @@ function farToCel (farenheit) {
 //////////////////////
 // Variables
 //////////////////////
-var count = 1
 
+var testimonialArray = document.getElementsByClassName('testimonial');
+
+// This number is used to refer to a value of the Array 
+// and to know what testimonial should be shown next.
+var i = 0;
+//This is the total amount of divs used in the DOM, 
+//it is used to determine if the testimonial can still grow. 
+var allTestimonials = testimonialArray.length;
 
 //////////////////////
 // Functions
 //////////////////////
-/*function slideshowNext() {
-   count = count + 1
-   if (count == 6){
-   	count = 1
-   }
 
-   ('.testimonial')
- }
+// function to show next image
+function slideshowNext() {
+
+	i +=1
+	if (i > (allTestimonials - 1)) {
+		i = 0
+	}
+	renderTestimonial(testimonialArray[i]);
+}
 
  function slideshowPrev() {
- 	count = count - 1
- 	if (count == 0) {
- 		count = 5
- 	}
- }
+   i -=1
+   if (i <= 0) {
+   	i = allTestimonials - 1;
+   }
+	//console.log(testimonialArray[i])
+	renderTestimonial(testimonialArray[i]);
+}
 
-function SlideShow (domId) {
+function renderTestimonial (testimonial) {
+	if (testimonial.css('display', 'block')) {
+		testimonial.style.display = 'none';
+	}
+	testimonial.style.display = 'block';
+}
+
+/*function SlideShow (domId) {
     this.container = document.getElementById(domId);
     this.pictures = [];
     this.currentPicture = 0;
 };*/
-var testimonialElements_i = 0;
-var testimonial_elem;
-
-var testimonialElements = document.getElementsByClassName('testimonial');
-
-// I want to show the first array element and hide the rest
-
-//
 
 
 //////////////////////
 // click events
 //////////////////////
 
- /*$(".next").on("click",function() {
+ $(".next").on("click",function() {
  	slideshowNext();
+ 	//console.log('testing');
  })
 
  $(".prev").on("click",function() {
  	slideshowPrev();
-
- })*/
+ 	//console.log('testing');
+ })
